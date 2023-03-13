76ad3a251d59b365d7ae3b30d23d7639865a1b54
Jose Felix <joseenrfelixesp@gmail.com>
2023-03-13 12:34:57 -0700

    Final del primer chapter

---
+++ chapter1/BankAccount.java
@@ -0,0 +1,20 @@
+package chapter1;
+class BankAccount{
+    private double balance;
+
+    public BankAccount(double openingBalance){
+        balance=openingBalance;
+    }
+
+    public void deposit(double amount){
+        balance=balance+amount;
+    }
+
+    public void withdraw(double amount){
+        balance=balance-amount;
+    }
+
+    public void display(){
+        System.out.println("balance="+balance);
+    }
+}
